3fa428a16be629e8d93474a98a7ee964443984f1
aaqib <aaqibnadeem123@gmail.com>
2022-11-06 03:11:09 +0500

    border styles card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,115 +1,120 @@
 <template>
     <div class="flex flex-col uppercase mt-2">
+        <div class="border-l border-l-3 mb-3 relative">
+            <img src="../assets/images/l-shape.svg" alt="" class="absolute top-0 right-0" />
+            <div class="bg-[#262626] rounded-tr-lg">
+                <div class="p-4 mb-2">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
+                            <div class="border border-scent2  px-2">
+                                01
+                            </div>
+                            <p>現貨策略</p>
+                        </div>
+                        <div class="">$973.23K</div>
+                    </div>
+                    <div class="flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
+                        <div class="flex-1 bg-[#4A4D50]">
+                            <div class="tones-progress-bar" style="width: 45%"></div>
+                        </div>
+                        
+                        <div class="text-tint-white normal-case">Ratio 37.21%</div>
+                    </div>
+                </div>
+
+                <div class="p-4">
+                    <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
+                        <img src="../assets/images/yellow.svg" alt="" />
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg normal-case">Earn</p>
+                            <p class="font-bold xl:text-3xl text-lg">$33,261.11</p>
+                        </div>
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg">Apy</p>
+                            <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">17.23%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="border-l border-l-3 px-2 mb-3 relative">
+            <img src="../assets/images/l-shape.svg" alt="" class="absolute top-0 right-0" />
+            <div class="pt-2 pb-2">
+                <div class="bg-[#121212] p-4 mb-2">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
+                            <div class="border border-scent2  px-2">
+                                02
+                            </div>
+                            <p>量化交易</p>
+                        </div>
+                        <div class="">$973.23K</div>
+                    </div>
         
-        <div class=" mb-3 bg-[#262626] relative rounded-tr-lg">
-            <img src="../assets/images/l-shape.svg" alt="" class="absolute top-0 right-0" />
-            <div class="p-4 mb-2">
-                <div class="flex flex-wrap items-center justify-between">
-                    <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
-                        <div class="border border-scent2  px-2">
-                            01
+                    <div class="bg-[#121212] flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
+                        <div class="flex-1 bg-[#4A4D50]">
+                            <div class="tones-progress-bar" style="width: 45%"></div>
                         </div>
-                        <p>現貨策略</p>
+                        
+                        <div class="text-tint-white normal-case">Ratio 42.19%</div>
                     </div>
-                    <div class="">$973.23K</div>
-                </div>
-                <div class="flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
-                    <div class="flex-1 bg-[#4A4D50]">
-                        <div class="tones-progress-bar" style="width: 45%"></div>
-                    </div>
-                    
-                    <div class="text-tint-white normal-case">Ratio 37.21%</div>
-                </div>
-            </div>
 
-            <div class="p-4">
-                <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
-                    <img src="../assets/images/yellow.svg" alt="" />
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg normal-case">Earn</p>
-                        <p class="font-bold xl:text-3xl text-lg">$33,261.11</p>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg">Apy</p>
-                        <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">17.23%</p>
+                </div>
+                
+                <div class="bg-[#121212] p-4">
+                    <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
+                        <img src="../assets/images/yellow.svg" alt="" />
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg normal-case">Earn</p>
+                            <p class="font-bold xl:text-3xl text-lg">$109,716.27</p>
+                        </div>
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg">Apy</p>
+                            <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">4.21%</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="mb-3 relative pt-2 pr-2">
+        <div class="border-l border-l-3 px-2 mb-3 relative">
             <img src="../assets/images/l-shape.svg" alt="" class="absolute top-0 right-0" />
-            <div class="bg-[#121212] p-4 mb-2">
-                <div class="flex flex-wrap items-center justify-between">
-                    <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
-                        <div class="border border-scent2  px-2">
-                            02
+            <div class="pt-2 pb-2">
+
+                <div class="bg-[#121212] p-4 mb-2">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
+                            <div class="border border-scent2  px-2">
+                                03
+                            </div>
+                            <p>質押借貸</p>
                         </div>
-                        <p>量化交易</p>
+                        <div class="">$973.23K</div>
                     </div>
-                    <div class="">$973.23K</div>
-                </div>
-    
-                <div class="bg-[#121212] flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
-                    <div class="flex-1 bg-[#4A4D50]">
-                        <div class="tones-progress-bar" style="width: 45%"></div>
-                    </div>
-                    
-                    <div class="text-tint-white normal-case">Ratio 42.19%</div>
-                </div>
-
-            </div>
-            
-            <div class="bg-[#121212] p-4">
-                <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
-                    <img src="../assets/images/yellow.svg" alt="" />
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg normal-case">Earn</p>
-                        <p class="font-bold xl:text-3xl text-lg">$109,716.27</p>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg">Apy</p>
-                        <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">4.21%</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="mb-3 relative pt-2 pr-2">
-            <img src="../assets/images/l-shape.svg" alt="" class="absolute top-0 right-0" />
-
-            <div class="bg-[#121212] p-4 mb-2">
-                <div class="flex flex-wrap items-center justify-between">
-                    <div class="flex items-center space-x-2 text-scent2 font-bold text-lg">
-                        <div class="border border-scent2  px-2">
-                            03
+        
+                    <!-- lay 2 -->
+                    <div class="bg-[#121212] flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
+                        <div class="flex-1 bg-[#4A4D50]">
+                            <div class="tones-progress-bar" style="width: 45%"></div>
                         </div>
-                        <p>質押借貸</p>
+                        
+                        <div class="text-tint-white normal-case">Ratio 20.6%</div>
                     </div>
-                    <div class="">$973.23K</div>
-                </div>
-    
-                <!-- lay 2 -->
-                <div class="bg-[#121212] flex flex-wrap items-center justify-between xl:space-x-40 space-x-10 mt-3.5">
-                    <div class="flex-1 bg-[#4A4D50]">
-                        <div class="tones-progress-bar" style="width: 45%"></div>
-                    </div>
-                    
-                    <div class="text-tint-white normal-case">Ratio 20.6%</div>
-                </div>
 
-            </div>
-            
-            <div class="bg-[#121212] p-4">
-                <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
-                    <img src="../assets/images/yellow.svg" alt="" />
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg normal-case">Earn</p>
-                        <p class="font-bold xl:text-3xl text-lg">$8,203.05</p>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="font-bold text-lg">Apy</p>
-                        <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">12.3%</p>
+                </div>
+                
+                <div class="bg-[#121212] p-4">
+                    <div class=" flex items-center justify-between xl:space-x-10 space-x-5">
+                        <img src="../assets/images/yellow.svg" alt="" />
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg normal-case">Earn</p>
+                            <p class="font-bold xl:text-3xl text-lg">$8,203.05</p>
+                        </div>
+                        <div class="flex flex-col">
+                            <p class="font-bold text-lg">Apy</p>
+                            <p class="font-bold xl:text-3xl text-lg text-[#18FE4B]">12.3%</p>
+                        </div>
                     </div>
                 </div>
             </div>
